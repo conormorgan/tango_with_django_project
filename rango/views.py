c828a718687ae3337ea6047c57fde22184b95dc3
Conor <2199772m@student.gla.ac.uk>
2018-01-26 13:09:17 +0000

    End of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,11 @@ def index(request):
 
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
+
+    context_dict = {'boldmessage': "This tutorial has been put together by Conor Morgan"}
+
+    return render(request, 'rango/about.html', context=context_dict)
